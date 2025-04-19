d1e25ce5967d77bd771f951b5195e63d0099049e
Crowther <happymakers338@gmail.com>
2025-04-19 14:15:12 +0100

    hamburger working

--- script.js
+++ script.js
@@ -3,10 +3,3 @@ document.getElementById('hamburger').addEventListener('click', function() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('active');
 });
-
-const hamburger = document.getElementById('hamburger');
-const nav = document.querySelector('nav');
-
-hamburger.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
